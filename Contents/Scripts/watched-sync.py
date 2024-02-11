dfaff5beb42abea3d3bf745586a570f8bd69b1da
natyusha <985941+natyusha@users.noreply.github.com>
2024-02-10 22:57:50 -0500

    watched-sync.py: add option for multiple libraries at once

--- Contents/Scripts/watched-sync.py
+++ Contents/Scripts/watched-sync.py
@@ -15,10 +15,12 @@ Requirements:
   - Python 3.7+, Python-PlexAPI (pip install plexapi), Requests Library (pip install requests), Plex, ShokoRelay, Shoko Server
 Preferences:
   - Before doing anything with this script you must enter your Plex and Shoko Server information into the Prefs below.
-  - If you want to track watched states from managed/home accounts on your Plex server you can enter them as a python list under Plex_ExtraUsers
-      - It must be a list to work e.g. "'Plex_ExtraUsers': ['User1', 'User2']" leave it as None otherwise
+  - If your anime is split across multiple libraries they can all be added in a python list under "Plex_LibraryNames".
+      - It must be a list to work e.g. "'Plex_LibraryNames': ['Anime Shows', 'Anime Movies']"
+  - If you want to track watched states from managed/home accounts on your Plex server you can add them to "Plex_ExtraUsers" following the same list format as above.
+      - Leave it as "None" otherwise.
 Usage:
-  - Run in a terminal (watched-sync.py).
+  - Run in a terminal (watched-sync.py)
 Behaviour:
   - Due to the potential for losing a huge amount of data removing watch states has been omitted from this script.
 """
@@ -28,7 +30,7 @@ Prefs = {
     'Plex_Username': 'Default',
     'Plex_Password': '',
     'Plex_ServerName': 'Media',
-    'Plex_LibraryName': 'Anime',
+    'Plex_LibraryNames': ['Anime'],
     'Plex_ExtraUsers': None,
     'Shoko_Hostname': '127.0.0.1',
     'Shoko_Port': 8111,
@@ -56,7 +58,7 @@ if Prefs['Plex_ExtraUsers'] is not None:
         extra_users = [admin.user(username) for username in Prefs['Plex_ExtraUsers']]
         data = [admin.query(f'https://plex.tv/api/home/users/{user.id}/switch', method=admin._session.post) for user in extra_users]
         for userID in data: accounts.append(MyPlexAccount(token=userID.attrib.get('authenticationToken')))
-    except Exception as error: # if the extra users can't be found show an error but continue
+    except Exception as error: # if the extra users can't be found show an error and continue
         print(f'{error_prefix}Failed:', error)
 
 # grab a shoko api key using the credentials from the prefs
@@ -70,7 +72,7 @@ if 'status' in auth and auth['status'] in (400, 401):
     exit(1)
 
 # loop through all of the accounts listed and sync watched states
-print_f('\n┌ShokoRelay Watched Sync: Checking for Plex Episodes Not Watched in Shoko...')
+print_f('\n┌ShokoRelay Watched Sync')
 for account in accounts:
     try:
         plex = account.resource(Prefs['Plex_ServerName']).connect()
@@ -78,24 +80,26 @@ for account in accounts:
         print(f'└{error_prefix}Failed: Server Name Not Found')
         exit(1)
 
-    try:
-        anime = plex.library.section(Prefs['Plex_LibraryName'])
-    except Exception:
-        print(f'└{error_prefix}Failed: Library Name Not Found')
-        exit(1)
+    # loop through the configured libraries
+    for library in Prefs['Plex_LibraryNames']:
+        print_f(f'├┬Querying: {account} @ {Prefs['Plex_ServerName']}/{library}')
+        try:
+            anime = plex.library.section(library)
+        except Exception as error:
+            print(f'│{error_prefix}Failed:', error)
+            continue
 
-    print_f(f'├┬Querying {account}')
-    # loop through all unwatched episodes in the plex library
-    for episode in anime.searchEpisodes(unwatched=False):
-        for episode_path in episode.iterParts():
-            filepath = os.path.sep + os.path.basename(episode_path.file) # add a path separator to the filename to avoid duplicate matches
-            path_ends_with = requests.get(f'http://{Prefs['Shoko_Hostname']}:{Prefs['Shoko_Port']}/api/v3/File/PathEndsWith?path={urllib.parse.quote(filepath)}&limit=0&apikey={auth['apikey']}').json()
-            if path_ends_with[0]['Watched'] == None: 
-                print_f(f'│├─Relaying: {filepath} → {episode.title}')
-                try:
-                    for EpisodeID in path_ends_with[0]['SeriesIDs'][0]['EpisodeIDs']:
-                        requests.post(f'http://{Prefs['Shoko_Hostname']}:{Prefs['Shoko_Port']}/api/v3/Episode/{EpisodeID['ID']}/Watched/true?apikey={auth['apikey']}')
-                except Exception as error:
-                    print(f'││{error_prefix}─Failed: Make sure that the video file listed above is matched by Shoko\n', error)
-    print_f(f'│└─Finished {account}')
-print_f(f'└─Finished!')
+        # loop through all unwatched episodes in the plex library
+        for episode in anime.searchEpisodes(unwatched=False):
+            for episode_path in episode.iterParts():
+                filepath = os.path.sep + os.path.basename(episode_path.file) # add a path separator to the filename to avoid duplicate matches
+                path_ends_with = requests.get(f'http://{Prefs['Shoko_Hostname']}:{Prefs['Shoko_Port']}/api/v3/File/PathEndsWith?path={urllib.parse.quote(filepath)}&limit=0&apikey={auth['apikey']}').json()
+                if path_ends_with[0]['Watched'] == None: 
+                    print_f(f'│├─Relaying: {filepath} → {episode.title}')
+                    try:
+                        for EpisodeID in path_ends_with[0]['SeriesIDs'][0]['EpisodeIDs']:
+                            requests.post(f'http://{Prefs['Shoko_Hostname']}:{Prefs['Shoko_Port']}/api/v3/Episode/{EpisodeID['ID']}/Watched/true?apikey={auth['apikey']}')
+                    except Exception as error:
+                        print(f'││{error_prefix}─Failed: Make sure that the video file listed above is matched by Shoko\n', error)
+        print_f(f'│└─Finished!')
+print(f'└─Watched Sync Complete')
