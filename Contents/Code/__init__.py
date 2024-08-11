76c57e3e59a98d9d3aaf6623d0b77ddd89feea8c
natyusha <985941+natyusha@users.noreply.github.com>
2024-08-11 07:09:14 -0400

    Remove Support for Banner Images
    These are unsupported by TMDB and almost completely deprecated in Plex

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -203,7 +203,6 @@ class ShokoRelayAgent:
         # Get Posters & Backgrounds
         images = try_get(series_data, 'Images', {})
         self.metadata_add(metadata.posters, try_get(images, 'Posters', []))
-        self.metadata_add(metadata.banners, try_get(images, 'Banners', []))
         self.metadata_add(metadata.art, try_get(images, 'Backdrops', []))
 
         # Get Cast & Crew
