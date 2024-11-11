0602ee7ef4f948bd051994a2acf9e88c491fbef8
natyusha <985941+natyusha@users.noreply.github.com>
2024-11-11 15:38:16 -0500

    Add TMDB episode title override for "Short #"

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -284,8 +284,8 @@ class ShokoRelayAgent:
                         if original_title == 'Complete Movie': ep_title = re.sub(r'(:? The)?( Movie| Motion Picture)', '', ep_title)    # Reduce redundant movie descriptors
                         ep_title += ' — ' + original_title
 
-                # TMDB episode title override (if the episode title is Episode/Volume/Special/Movie [S]# on AniDB excluding number 0) and there is a TMDB match
-                default_titles = r'^(Episode|Volume|Special|(Short )?Movie) [S0]?[1-9][0-9]*$' # Regex pattern for default episode titles
+                # TMDB episode title override if the episode title is ambiguous and enumerated on AniDB (excluding number 0) and there is a TMDB match
+                default_titles = r'^(Episode|Volume|Special|Short|(Short )?Movie) [S0]?[1-9][0-9]*$' # Regex pattern for default episode titles
                 if tmdb_ep_title and re.match(default_titles, ep_title) and not re.match(default_titles, tmdb_ep_title): ep_title_mod, ep_title, lang = '(TMDB Override) [LANG]: ', tmdb_ep_title, 'shoko'
 
                 ep_object.title = ep_title
